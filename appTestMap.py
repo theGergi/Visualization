70c1f8b93b5d926dd6de764e85af9c5429931cff
Muhammad Rafi Pramanta Hidayat <rzmdlu@gmail.com>
2023-01-20 17:19:20 +0100

    Add files via upload

--- appTestMap.py
+++ appTestMap.py
@@ -1,15 +1,20 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, State
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 from dash.exceptions import PreventUpdate
 import numpy as np
+import re
 
 def clean(x):
     x = x.replace("$", "").replace(" ", "")
     if "," in x:
         x = x.replace(",", "")
+    return int(x)
+
+def cleanServiceFee(x):
+    x = x.replace("$", "").replace(" ", "")
     return int(x)
 
 df = pd.read_csv('airbnb_open_data.csv', usecols=['NAME','host id', 'host_identity_verified','host name',
@@ -22,6 +27,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 px.set_mapbox_access_token("pk.eyJ1IjoibXJhZmlwaCIsImEiOiJjbGMzdWZ0MTIwNmt5M3B0ODNnbzF1a3d2In0.7VgLitY9OXxhPSxlxJglfQ")
 
 app = Dash(__name__,external_stylesheets=external_stylesheets)
+
+app.config.suppress_callback_exceptions = True
 
 styles = {
     'pre' : {
@@ -39,7 +46,14 @@ server = app.server
 df_clean = df.copy()
 df_clean = df_clean.dropna().reset_index(drop=True)
 df_clean['price'] = df_clean['price'].apply(clean)
+df_clean['service fee'] = df_clean['service fee'].apply(cleanServiceFee)
 df_clean['neighbourhood group'] = df_clean['neighbourhood group'].replace('brookln', 'Brooklyn')
+
+# Separate these values into different bins
+df_clean['bin'] = pd.cut(x=df_clean['service fee'], bins=[0, 10, 60, 120, 180, 240])
+df_clean['bin'] = df_clean['bin'].astype(str)
+df_clean['bin_price'] = pd.cut(x=df_clean['price'], bins=5, precision=0)
+df_clean['bin_price'] = df_clean['bin_price'].astype(str)
 
 # Make the layout 
 app.layout = html.Div(children=[
@@ -47,7 +61,7 @@ app.layout = html.Div(children=[
         className='row', children=[
             html.Div(className='four columns div-user-controls', children=[
                 html.H2('Dash - Airbnb Listings'),
-                html.P('''Filter Options'''),
+                html.P('''Filtering Options'''),
                 html.Hr(),
                 dcc.Dropdown(
                     [{'label': 'Location', 'value': 'Location'},
@@ -72,13 +86,22 @@ app.layout = html.Div(children=[
                     options=df_clean['room type'].unique(),
                     style= {'display': 'none'},
                     value='Private room',
-                )
+                ),
+                dcc.Dropdown(
+                    options=['$50 - $280', '$281 - $509', '$510 - $739', '$740 - $969', '$970 - $1200'],
+                    id='dropdown-price',
+                    value='$50 - $280',
+                    disabled=True,
+                ),
             ]),
 
-            html.Div(className='eight columns div-for-charts-bg-grey', children=[
+            html.Div(className='six columns div-for-charts-bg-grey', children=[
                 dcc.Graph(
                     id='hexbin-mapbox',
-                ),
+                    style={'display': 'inline-block'}
+                ),html.Div(className='five columns div-for-charts-bg-grey', children=[
+
+                ], id='grouped-bar-chart', style={'display': 'inline-block'}),
             ])
         ], 
     )
@@ -113,8 +136,10 @@ def update_graph(value, value_neighbour, value_room):
             original_data_marker=dict(size=4, opacity=0.2, color="deeppink"),
             zoom=8,
         )
+
     fig.update_layout(margin=dict(b=0, t=0, l=0, r=0))
     return fig, disabled_neighbour, disabled_room
+
 
 def make_hexbin(df, setOriginalData):
     return ff.create_hexbin_mapbox(
@@ -123,7 +148,71 @@ def make_hexbin(df, setOriginalData):
             min_count=1, show_original_data=setOriginalData, 
             original_data_marker=dict(size=4, opacity=0.2, color="deeppink"),
             zoom=8,
-        )
+    )
+
+@app.callback(
+    [Output('grouped-bar-chart', 'children'),
+    Output('dropdown-price', 'style'),
+    Output('dropdown-price', 'disabled')],
+    [Input('dropdown-menu', 'value'),
+    Input('dropdown-price', 'value')],
+    [State('grouped-bar-chart', 'children')]
+)
+def update_grouped(value, priceRange, children):
+    disabled_bar = {'disabled':'none'}
+    disable_drop = True
+    if (value == 'price'):
+        disabled_bar = {'disabled':'block'}
+        disable_drop = False
+        filtered_df = reviewPriceRange(df_clean[df_clean["bin_price"]==priceParser(priceRange)])
+        if children:
+            children[0]["props"]["figure"] = px.bar(filtered_df, x='range', y='bin', color='review', 
+                        barmode="group", text='bin')
+            children[0]["props"]["figure"].update_layout(xaxis_title="Service fee range", yaxis_title="Total Review")
+        else:
+            fig_second = px.bar(filtered_df, x='range', y='bin', color='review', 
+                        barmode="group", text='bin')
+            fig_second.update_layout(xaxis_title="Service fee range", yaxis_title="Total Review")
+            children.append(
+                    dcc.Graph(
+                        figure=fig_second)
+            )
+    return children, disabled_bar, disable_drop
+
+def reviewPriceRange(df_clean):
+    df1 = pd.DataFrame(df_clean.groupby(by=['bin','review rate number'])['bin'].count())
+    df1.index.names = ['range', 'review']
+    df1.reset_index(inplace=True)
+    df1['review'] = df1['review'].astype(str)
+    ranges = df1['range'].unique()
+    priceRanges = ['$0 - $10', '$10 - $60', '$60 - $120', '$120 - $180', '$180 - $240']
+
+    for element in ranges:
+        inputToken = element
+        if element == '(0, 10]':
+            df1['range'] = df1['range'].str.replace(re.escape(inputToken), priceRanges[0])
+        if element == '(10, 60]':
+            df1['range'] = df1['range'].str.replace(re.escape(inputToken), priceRanges[1])
+        if element == '(60, 120]':
+            df1['range'] = df1['range'].str.replace(re.escape(inputToken), priceRanges[2])
+        if element == '(120, 180]':
+            df1['range'] = df1['range'].str.replace(re.escape(inputToken), priceRanges[3])
+        if element == '(180, 240]':
+            df1['range'] = df1['range'].str.replace(re.escape(inputToken), priceRanges[4])
+
+    return df1
+
+def priceParser(priceRange):
+    if priceRange == '$50 - $280':
+        return "(49.0, 280.0]"
+    elif priceRange == '$281 - $509':
+        return "(281.0, 510.0]"
+    elif priceRange == '$510 - $739':
+        return "(510.0, 740.0]"
+    elif priceRange == '$740 - $969':
+        return "(740.0, 970.0]"
+    else:
+        return "(970.0, 1200.0]"
     
 
 if __name__ == '__main__':
